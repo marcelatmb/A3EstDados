23eac8d4614b9dbebaa58859615cf780cc5c95f0
diego-rng <diego.renova55@gmail.com>
2025-11-21 17:11:53 -0300

    a

--- tests/test_arvore.py
+++ tests/test_arvore.py
@@ -1,25 +1,25 @@
-from src.arvore import No
+from src.arvore import no
 
 
 def test_no_simples():
-    n = No("3")
+    n = no("3")
     assert n.valor == "3"
     assert n.esquerda is None
     assert n.direita is None
 
 
 def test_no_com_filhos():
-    n = No("+", No("2"), No("3"))
+    n = no("+", no("2"), no("3"))
     assert n.valor == "+"
     assert n.esquerda.valor == "2"
     assert n.direita.valor == "3"
 
 
 def test_impressao_lisp_soma():
-    n = No("+", No("3"), No("4"))
+    n = no("+", no("3"), no("4"))
     assert n.to_lisp() == "(+ 3 4)"
 
 
 def test_impressao_lisp_expressao_aninhada():
-    n = No("*", No("+", No("3"), No("4")), No("5"))
+    n = no("*", no("+", no("3"), no("4")), no("5"))
     assert n.to_lisp() == "(* (+ 3 4) 5)"
